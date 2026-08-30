ab59224f6ce898eda62c1b377f6d98e824e9a7ac
dn9713 <developerdn97@gmail.com>
2026-08-30 22:33:38 +0300

    Update socket_cache.js

--- packages/core/src/_common/base/socket_cache.js
+++ packages/core/src/_common/base/socket_cache.js
@@ -1,39 +1,19 @@
-import moment from 'moment';
-import { isEmptyObject, getPropertyValue, LocalStore } from '@deriv/shared';
-import { getStaticHash } from '_common/utility';
+const moment = require('moment');
+const { isEmptyObject, getPropertyValue, LocalStore } = require('@deriv/shared');
+const { getStaticHash } = require('_common/utility');
 
 /*
  * Caches WS responses to reduce delay time and number of requests
  * Stores data in LocalStore which is the first one available in: localStorage, sessionStorage, InScriptStore
- *
- * 1. It caches only the response of those calls which determined in `config`
- * 2. It doesn't cache responses which returned error
- * 3. The value is requested by BinarySocket,
- * if this returns a value according to the logic here, socket code take it as response
- * but also triggers an async `send` request, to keep the cache updated for next time
- * 4. Uses client's time to set and check for expiry, as the expire durations are not so long to need a more precise one
- * (And doesn't worth to wait for the response of time call)
- * 5. Some responses should be cached by a particular value from request (e.g. contracts_for_frxAUDJPY)
- * so there can be more than one value for a particular call
- * 6. Clears the whole cache regardless their expire time on the following events:
- * 6.1. Client changes: login / logout / switch loginid
- * 6.2. Detect a new release (static hash changed)
  */
 const SocketCache = (() => {
-    // keys are msg_type
-    // expire: how long to keep the value (in minutes)
     const config = {
         proposal_open_contract: { expire: 10 },
         contracts_for: { expire: 10 },
         trading_times: { expire: 120 },
-        // TODO: Enable statement and trade table caching once we have UI design for handling
-        // transitions between cached table and newly added data to table
-        // statement : { expire: 10 },
-        // profit_table : { expire: 10 },
     };
 
     const storage_key = 'ws_cache';
-
     let data_obj = {};
 
     const msg_type_mapping = {
@@ -43,23 +23,19 @@ const SocketCache = (() => {
 
     const set = (key, response) => {
         const msg_type = msg_type_mapping[response.msg_type] || response.msg_type;
-        // check if response has subscription, since we only want to cache non-streaming responses
-        // sold proposal_open_contract response can be cached
         if (response.subscription) {
             const can_cache = msg_type === 'proposal_open_contract' && response.proposal_open_contract.is_sold;
             if (!can_cache) return;
         }
 
         if (response?.echo_req?.end === 'latest') return;
-
         if (!config[msg_type]) return;
-        // prevent unwanted page behaviour
-        // if a cached version already exists but it gives an error after being called for updating the cache
+
         const cached_response = get(response.echo_req) || {};
         const cached_message = cached_response[msg_type];
         const new_message = response[msg_type];
 
-        const has_error_or_missing = response.error; // || !(msg_type in response);
+        const has_error_or_missing = response.error;
         const has_new_value = cached_message && isEmptyValue(cached_message) && !isEmptyValue(new_message);
         const has_old_cache = cached_message && isEmptyValue(new_message) && !isEmptyValue(cached_message);
         const has_valid_cache = !isEmptyValue(cached_response) && !cached_response.error;
@@ -82,13 +58,9 @@ const SocketCache = (() => {
     const isEmptyValue = data => {
         let is_empty_data = false;
         if (Array.isArray(data)) {
-            if (!data.length) {
-                is_empty_data = true;
-            }
+            if (!data.length) is_empty_data = true;
         } else if (typeof data === 'object' && data !== null) {
-            if (!Object.keys(data).length) {
-                is_empty_data = true;
-            }
+            if (!Object.keys(data).length) is_empty_data = true;
         }
         return is_empty_data;
     };
@@ -100,7 +72,6 @@ const SocketCache = (() => {
         }
 
         if (data_obj.static_hash !== getStaticHash()) {
-            // new release
             clear();
         }
     };
@@ -109,43 +80,31 @@ const SocketCache = (() => {
 
     const get = key => {
         reloadDataObj();
-
         const response_obj = getData(key);
-
         let response;
         if (moment().isBefore(response_obj.expires)) {
             response = response_obj.value;
         } else {
-            // remove if expired
             remove(key);
         }
-
         return response;
     };
 
     const getByMsgType = msg_type => {
         reloadDataObj();
-
         const key = Object.keys(data_obj).find(k => getData(k).msg_type === msg_type);
-
         if (!key) return undefined;
-
         const response_obj = getData(key);
-
         let response;
         if (moment().isBefore(response_obj.expires)) {
             response = response_obj.value;
         } else {
-            // remove if expired
             remove(key);
         }
-
         return response;
     };
 
-    const has = key => {
-        return !!get(key);
-    };
+    const has = key => !!get(key);
 
     const remove = (key, should_match_all) => {
         if (should_match_all) {
@@ -175,5 +134,4 @@ const SocketCache = (() => {
     };
 })();
 
-export default SocketCache;
-
+module.exports = SocketCache;
